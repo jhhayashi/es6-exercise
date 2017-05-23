ca0717140b5909158bf45f68df3f254a860f9666
Jordan Hayashi <jhhayashi@gmail.com>
2017-05-23 17:12:04 -0400

    Add one second delay to server responses

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const ERROR_CODES = [0, 400, 403, 404, 500]
 
 const app = express()
 
+app.use((req, res, next) => {setTimeout(next, 1000)})
+
 if (process.env.CHALLENGE_MODE) {
   app.use((req, res, next) => {
     if (Math.round(Math.max(Math.random() - 0.25, 0))) {
